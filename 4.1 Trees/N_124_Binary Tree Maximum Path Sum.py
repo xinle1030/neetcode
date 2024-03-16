a887d689a2e8b7c8bd37c71615ef75994c42a3dc
xinle1030 <xinlelam1030@gmail.com>
2024-03-16 17:48:30 +0800

    complete leetcode 124

---
+++ 4.1 Trees/N_124_Binary Tree Maximum Path Sum.py
@@ -0,0 +1,49 @@
+"""
+References: https://www.youtube.com/watch?v=Hr5cWUld4vU
+"""
+
+from typing import Optional, List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = [root.val]
+
+        def dfs(root):
+            if not root:
+                return 0
+            
+            leftMax = dfs(root.left)
+            rightMax = dfs(root.right)
+
+            # could be negative so nid compare with 0 again
+            leftMax = max(leftMax, 0)
+            rightMax = max(rightMax, 0)
+
+            # compute max path sum with split
+            res[0] = max(res[0], root.val + leftMax + rightMax)
+
+            # this function dfs should return path sum without splitting
+            return root.val + max(leftMax, rightMax)
+        
+        dfs(root)
+        return res[0]
+
+mySol = Solution()
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+print(mySol.maxPathSum(root))
+
+root = TreeNode(-10)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+print(mySol.maxPathSum(root))
