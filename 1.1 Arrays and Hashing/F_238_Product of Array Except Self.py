5f53f3a47288e6490dc311a8eece2861f320379e
xinle1030 <xinlelam1030@gmail.com>
2024-04-04 13:43:20 +0800

    update notes

--- 1.1 Arrays and Hashing/F_238_Product of Array Except Self.py
+++ 1.1 Arrays and Hashing/F_238_Product of Array Except Self.py
@@ -1,5 +1,9 @@
 """
 References: https://www.youtube.com/watch?v=bNvIQI2wAjk
+
+At the end of this pass, 
+res[i] contains the product of all elements to the left of nums[i] multiplied by all elements to the right of nums[i]
+, effectively excluding nums[i] itself.
 """
 
 from typing import List
@@ -12,7 +16,7 @@ class Solution:
         for i in range(len(nums)):
             res[i] = prefix
             prefix *= nums[i]
-        
+
         postfix = 1
         for i in range(len(nums) - 1, -1, -1):
             res[i] *= postfix
